c7a85dfcaf8e92df974989dc45d16ecf0200a98d
EduardoTIz <eduardosouza2308x@gmail.com>
2024-11-06 15:29:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Teste")
+const email = "ceciliameiochata@gmail.com"
+const senha = "fanfiqueira é vida"
+
+localStorage.setItem(email, senha)
+
+const emailInput = document.getElementById("email")
+const senhaInput = document.getElementById("senha")
+const formulario = document.getElementById("formulario_login")
+
+formulario.addEventListener("submit", (event) => {
+    event.preventDefault()
+    
+    const emailDigitado = emailInput.value
+    const senhaDigitado = senhaInput.value
+
+
+    console.log(emailDigitado)
+    console.log(senhaDigitado)
+})
